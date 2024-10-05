5fa642d50c31d7471bdbcac216d985e736e5d09f
Chandrakanth540 <104191491+Chandrakanth540@users.noreply.github.com>
2024-10-05 07:15:07 +0530

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,6 +1,6 @@
-set -u # Treat unset variables as an error
-: "${CONTAINER_REGISTRY:?CONTAINER_REGISTRY is not set}"
-: "${VERSION:?VERSION is not set}"
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
 
 # Build and tag images using Docker Compose
 docker-compose -f ../docker-compose.yml build
